af21833966c896d5adce086cebec1652c1d45089
danrocsan <danrocsan@gmail.com>
2024-05-17 13:56:22 -0300

    Ligando o front-end ao back end pela api.

--- crud-spring/src/main/java/com/rocha/controller/CoursesController.java
+++ crud-spring/src/main/java/com/rocha/controller/CoursesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rocha.model.Course;
@@ -20,7 +21,7 @@ public class CoursesController {
 
 
     @GetMapping
-    public List<Course> list(){
+    public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
     }
     
